0612710de7479cac4772ce6dab703969a2813f42
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-13 11:03:15 -0500

    making nextquestion function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,51 @@
+var pairs = [
+    {
+        question: "2 + 2 =",
+        answers: [
+            {
+                answer: 1,
+                status: true,
+            },
+            {
+                answer: 2,
+                status: false,
+            },
+            {
+                answer: 3,
+                status: false,
+            },
+            {
+                answer: 4,
+                status: false,
+            },
+        ],
+        displayed: false
+    }
+
+]
+
 $(document).ready(function () {
     $("#quiz").hide();
     $("#score").hide();
-    var pairs = [
-        {
-            question: "2 + 2 =",
-            answers: [
-                {
-                    answer: 1,
-                    status: true,
-                },
-                {
-                    answer: 2,
-                    status: true,
-                },
-                {
-                    answer: 3,
-                    status: false,
-                },
-                {
-                    answer: 4,
-                    status: false,
-                },
-            ]
-        }
 
-    ]
     
-    for ( var i = 0; i < pairs.length ; i++) {
-        $("#question").append("<p id='question-" + i + "'>" + pairs[i].question + "</p>");
-        for (var j = 0; j < pairs[i].answers.length; j++) {
-            $("#answers").append("<a class= 'btn btn-primary' id='option-" + j + "'>" + pairs[i].answers[j].answer + "</a> <br>");
-        }
-        
-    }
 })
-
 
 $(document).on("click", "#startbutton", function() {
 
     $("#start").hide();
     $("#quiz").show();
-
+    $("#question").append("<p id='question-" + 0 + "'>" + pairs[0].question + "</p>");
+    for (var j = 0; j < pairs[0].answers.length; j++) {
+        $("#answers").append("<a class= 'btn btn-primary' id='option-" + j + "'>" + pairs[0].answers[j].answer + "</a> <br>");
+    }
+    pairs[0].displayed = true;
 })
 
+function NextQuestion() {
+    
+
+}
 // $(document).on("click", ".answer", function() {
 //     for ( var i = 0; i < pairs.length ; i++) {
 //         console.log ($("#question-"+i)
