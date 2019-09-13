7694bb5d21423e3b6e1465cb1c7e83b60b3dcaa6
Kemp Atkinson <kempatkinson@gmail.com>
2019-09-12 20:59:46 -0500

    dynamic answers/question in put

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $("#quiz").hide();
+    $("#score").hide();
     var pairs = [
         {
-            question: "2 + 2",
+            question: "2 + 2 =",
             answers: [
                 {
                     answer: 1,
@@ -12,7 +14,7 @@ $(document).ready(function () {
                     status: true,
                 },
                 {
-                    answer: 2,
+                    answer: 3,
                     status: false,
                 },
                 {
@@ -23,23 +25,29 @@ $(document).ready(function () {
         }
 
     ]
+    
     for ( var i = 0; i < pairs.length ; i++) {
-        console.log(pairs[i])
+        $("#question").append("<p id='question-" + i + "'>" + pairs[i].question + "</p>");
         for (var j = 0; j < pairs[i].answers.length; j++) {
-            console.log(pairs[i].answers[j])
-
-
+            $("#answers").append("<a class= 'btn btn-primary' id='option-" + j + "'>" + pairs[i].answers[j].answer + "</a> <br>");
         }
         
     }
-
-
 })
 
 
 $(document).on("click", "#startbutton", function() {
 
-    $("#start").empty();
-    
+    $("#start").hide();
+    $("#quiz").show();
 
 })
+
+// $(document).on("click", ".answer", function() {
+//     for ( var i = 0; i < pairs.length ; i++) {
+//         console.log ($("#question-"+i)
+
+        
+//     }
+
+// })
